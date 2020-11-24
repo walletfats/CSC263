b4f1b316ce9cda7c31a27b7d613c461dbac6fafd
Jason <43427061+walletfats@users.noreply.github.com>
2020-11-24 15:39:57 -0500

    Add files via upload

---
+++ RoomDimension.h
@@ -0,0 +1,12 @@
+#ifndef ROOMDIMENSION_H_INCLUDED
+#define ROOMDIMENSION_H_INCLUDED
+
+class RoomDimension;
+
+class RoomDimension: public FeetInches {
+    protected:
+        feet;
+        inches;
+};
+
+#endif // ROOMDIMENSION_H_INCLUDED
